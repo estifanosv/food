ee242dc47bf9d0b78938d7cf4314c5c5a3aa20af
estifanosv <estifob@gmail.com>
2018-10-11 12:08:40 +0300

    to add

---
+++ tibet/file/C_/Users/Ken/Desktop/Tibeb web/wub.js
@@ -0,0 +1,8 @@
+
+document.getElementById("Date").innerHTML= new Date().toDatestring();
+/*var x1 = 34.00;
+var x2 = 35;
+var x3 = 3.14;
+//var y = 123e5;      // 12300000
+//var z = 123e-5;     // 0.00123
+document.getElementById("demo").innerHTML = x1 + "<br>" + x2 + "<br>" + x3;
